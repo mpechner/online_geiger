f6daad4c391f7c1163acb86ed1796b5f49d57260
Michael Pechner <mikey@mikey.com>
2020-11-22 20:29:42 -0800

    Have it working well.  Sending the counts fields also.

--- send.py
+++ send.py
@@ -33,7 +33,7 @@ def read_value():
         timeout=1
     )
     x=ser.readline()
-    return float(x.split(b',')[5])
+    return str(x).strip().split(',')
 
 parser = argparse.ArgumentParser(description="Will send telemetry for a might ohm geiger counter.")
 parser.add_argument('--call', required=True,help='callsign dash sid')
@@ -45,21 +45,25 @@ args = parser.parse_args()
 call = args.call
 password = args.passcode
 
+# CSV lines; CPS, #####, CPM, #####, uSv/hr, ###.##, SLOW|FAST|INST
 frames = [ 
 "{call}>APDW16:!3721.34N/12159.52WImightyohm Geiger, Santa Clara, CA",
-"{call}>APDW16::{call} :PARM.Geiger",
-"{call}>APDW16::{call} :UNIT.uSv/hr,",
-"{call}>APDW16::{call} :EQNS.0,1.0,0",
-"{call}>APDW16::{call} :BITS.11111111,background radiation",
+"{call}>APDW16::{call} :PARM.uSv/hr, Clicks/Sec, Clicks/Min",
+"{call}>APDW16::{call} :UNIT.uSv/hr,C/S,C/M",
+"{call}>APDW16::{call} :EQNS.0,1.0,0,0,1.0,0,0,1.0,0",
+"{call}>APDW16::{call} :BITS.11111111,Mighty Ohm Background Radiation",
 ]
 
 conn = aprs.TCP(call.encode('utf-8'), password.encode('utf-8'))
 conn.start() 
 for frame in frames:
-    #print(frame.format(call=call).encode('utf-8'))
+    print(frame.format(call=call).encode('utf-8'))
     conn.send(frame.format(call=call).encode('utf-8'))
 
 next_seq()
-conn.send("{call}>APDW16:T#{seq:03d},{val:04.2f}".format(call=call, seq=seq, val=read_value()).encode('utf-8'))
+vals=read_value()
+print("data", ','.join(vals))
+print("{call}>APDW16:T#{seq:03d},{val},{cps},{cpm},".format(call=call, seq=seq, val=vals[5].strip(),cps=vals[1].strip(),cpm=vals[3].strip()).encode('utf-8'))
+conn.send("{call}>APDW16:T#{seq:03d},{val},{cps},{cpm},".format(call=call, seq=seq, val=vals[5].strip(),cps=vals[1].strip(),cpm=vals[3].strip()).encode('utf-8'))
 
 
